bb3f1ac464880b44aca88b4dd0934ddba36c7e85
Diego <diegocorf123@gmail.com>
2021-11-09 11:19:17 -0600

    Start inicial settings

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/global.scss'
+
+const App = () => {
+    return(
+        <h1>Hola mundo, bienvenidos al curso practico de react</h1>
+    );
+}
+export default App;
