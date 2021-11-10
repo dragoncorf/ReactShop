722c8349922317dbc3d32dc9c1ad678352d4f54d
Diego <diegocorf123@gmail.com>
2021-11-10 11:00:10 -0600

    Add Challenge logic

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   mode: "development",
   resolve: {
@@ -30,7 +31,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.s[ac]ss$/,
+        test: /\.(css|scss)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
@@ -45,6 +46,7 @@ module.exports = {
     }),
   ],
   devServer: {
+    historyApiFallback: true,
     static: { directory: path.join(__dirname, "dist") },
     compress: true,
     port: 3005,
