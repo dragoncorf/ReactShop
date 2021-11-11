fbad471bb3c128d7f59d248cbbe51738d97d8c73
Diego <diegocorf123@gmail.com>
2021-11-11 10:11:42 -0600

    Add customHooks

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ProductItem from '../components/ProductItem';
-import '../styles/ProductList.scss';
+import React from "react";
+import ProductItem from "@components/ProductItem";
+import "../styles/ProductList.scss";
+import useGetProducts from "../hooks/useGetProducts";
+
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
-	return (
-		<section className="main-container">
-			<div className="ProductList">
-				<ProductItem />
-			</div>
-		</section>
-	);
-}
+  const products = useGetProducts(API);
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
